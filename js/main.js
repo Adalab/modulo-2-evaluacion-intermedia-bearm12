f7a9c5ec83ce33046169162d3dcb222b2fed0aad
bearm12 <bearm12493@gmail.com>
2024-02-22 09:08:06 +0000

    number check and clues

--- js/main.js
+++ js/main.js
@@ -1,2 +1,39 @@
 'use strict';
 
+const button = document.querySelector('.js-button');
+const numberInput = document.querySelector('.js-input');
+const clue = document.querySelector('.js-clue');
+const counterMessage = document.querySelector('.js-counter');
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+const randomNumber = getRandomNumber(100);
+console.log(`Mi número aleatorio es ${randomNumber}`);
+
+function checkNumber () {
+    const numberValue = numberInput.value;
+
+    if (parseInt(numberValue) < 1 || numberValue > 100) {
+        clue.innerHTML = 'El número debe estar entre 1 y 100';
+    } else if (parseInt(numberValue) === randomNumber) {
+        clue.innerHTML = '¡¡HAS GANADO CAMPEONA!!';
+    } else if (parseInt(numberValue) < randomNumber){
+        clue.innerHTML = 'Pista: Demasiado bajo';
+    } else if (parseInt(numberValue) > randomNumber){
+        clue.innerHTML = 'Pista: Demasiado alto';
+    }
+}
+
+function counter () {
+
+}
+
+function handleClick(event) {
+    event.preventDefault();
+    
+    checkNumber()
+}
+
+button.addEventListener('click', handleClick)
