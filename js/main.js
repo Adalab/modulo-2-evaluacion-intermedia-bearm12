35bc7eff1b7dcfbd26571b3f081bb415c77471d9
bearm12 <bearm12493@gmail.com>
2024-02-22 11:36:31 +0000

    restart

--- js/main.js
+++ js/main.js
@@ -4,24 +4,25 @@ const button = document.querySelector('.js-button');
 const numberInput = document.querySelector('.js-input');
 const clue = document.querySelector('.js-clue');
 const counterMessage = document.querySelector('.js-counter');
+const restart = document.querySelector('.js-restart')
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
 
-const randomNumber = getRandomNumber(100);
+let randomNumber = getRandomNumber(100);
 
 
 function checkNumber () {
-    const numberValue = numberInput.value;
+    const numberValue = parseInt(numberInput.value);
 
-    if (parseInt(numberValue) < 1 || numberValue > 100) {
+    if (numberValue < 1 || numberValue > 100) {
         clue.innerHTML = 'El número debe estar entre 1 y 100';
-    } else if (parseInt(numberValue) === randomNumber) {
+    } else if (numberValue === randomNumber) {
         clue.innerHTML = '¡¡HAS GANADO CAMPEONA!!';
-    } else if (parseInt(numberValue) < randomNumber){
+    } else if (numberValue < randomNumber){
         clue.innerHTML = 'Pista: Demasiado bajo';
-    } else if (parseInt(numberValue) > randomNumber){
+    } else if (numberValue > randomNumber){
         clue.innerHTML = 'Pista: Demasiado alto';
     }
 }
@@ -41,4 +42,14 @@ function handleClick(event) {
     counter();
 }
 
-button.addEventListener('click', handleClick)
+const handleRestart = () => {
+    i = 0
+    counterMessage.innerHTML = `Número de intentos: ${i}`;
+    clue.innerHTML = 'Pista: Escribe el número y dale a Prueba'
+    randomNumber = getRandomNumber(100);
+
+
+}
+
+button.addEventListener('click', handleClick);
+restart.addEventListener('click', handleRestart)
